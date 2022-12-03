9cb66ca96476f3f3ebc8d45c5beebb9f3ffff6dd
codehamsters <rohandeepbansal@gmail.com>
2022-12-03 21:46:25 +0530

    Update ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS with site URL

--- chatroom/settings.py
+++ chatroom/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-cc#lxxjskcp8!+tj*!jn89-i^&gh7)_%8mt6gd7+89hz13%2*^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['cacourse.info','www.cacourse.info','127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','52.198.118.142','www.dark-chatroom.me','dark-chatroom.me']
 
 
 # Application definition
